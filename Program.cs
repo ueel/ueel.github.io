dadbfdab0542e225660e99248d1b9f9a3d9563b3
ueel <jiwonbear@gmail.com>
2024-05-20 20:10:49 +0900

    changed projects to Radzen Blazor

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using YamsHouse;
+using Radzen;
+using Yamine;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+builder.Services.AddRadzenComponents();
+
+var app = builder.Build();
+
+await app.RunAsync();
